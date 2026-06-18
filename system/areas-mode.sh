85205067dd64c274a5dfdb84c532b587eabc970b
T4rg3n <vic.gombert@gmail.com>
2026-06-18 16:14:15 +0200

    add : areas touchpad mode

---
+++ system/areas-mode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Set the touchpad click method to areas (right click button on touchpad, opposed to double fingers)
+gsettings set org.gnome.desktop.peripherals.touchpad click-method 'areas'
