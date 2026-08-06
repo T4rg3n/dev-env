828f6eef2dc71b3347a8d4cba0a53f0ffed6987f
T4rg3n <vic.gombert@gmail.com>
2026-08-06 21:55:55 +0200

    fix : clipboard now clearing on session start

--- utils/clipboard.sh
+++ utils/clipboard.sh
@@ -5,8 +5,12 @@ if [ ! -d ~/.local/share/gnome-shell/extensions/ ]; then
     mkdir -p ~/.local/share/gnome-shell/extensions/
 fi
 
-# Clone the repo into ~
-git clone https://github.com/tudmotu/gnome-shell-extension-clipboard-indicator.git ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
+EXT_DIR=~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
+
+# Clone the repo if missing
+if [ ! -d "$EXT_DIR" ]; then
+    git clone https://github.com/tudmotu/gnome-shell-extension-clipboard-indicator.git "$EXT_DIR"
+fi
 
 # Enable the extension
 gnome-extensions enable clipboard-indicator@tudmotu.com
@@ -14,3 +18,11 @@ gnome-extensions enable clipboard-indicator@tudmotu.com
 # Remove the default keybinding
 gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>m']"
 
+# Clear history at session start (extension load / login / reboot),
+# not on a recurring mid-session timer.
+SCHEMA_DIR="$EXT_DIR/schemas"
+SCHEMA=org.gnome.shell.extensions.clipboard-indicator
+if [ -d "$SCHEMA_DIR" ]; then
+    gsettings --schemadir "$SCHEMA_DIR" set "$SCHEMA" clear-on-boot true
+    gsettings --schemadir "$SCHEMA_DIR" set "$SCHEMA" clear-history-on-interval false
+fi
